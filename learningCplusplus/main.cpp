1d7ef7101ebff241480a2104394bd69860a4faed
sleung852 <sleung852@gmail.com>
2018-10-11 18:40:18 +0800

    Initial Commit

---
+++ learningCplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learningCplusplus
+//
+//  Created by See Ho Leung on 11/10/2018.
+//  Copyright © 2018 See Ho Leung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
